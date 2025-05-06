e62c1504fb6bf8c5bbb7d210e5bbdfe2fa8c6257
jgjerde <jgjerde@umich.edu>
2025-05-06 12:34:35 -0400

    Corrected potential well. Working app without electric fields

--- applications/capillary_void/ICs_and_BCs.cc
+++ applications/capillary_void/ICs_and_BCs.cc
@@ -23,8 +23,9 @@ customPDE<dim, degree>::setInitialCondition([[maybe_unused]] const Point<dim>  &
     
         // int_width = 3*(userInputs.domain_size[0] / ((double) userInputs.subdivisions[0]) /
         //   std::pow(2.0, userInputs.refine_factor));
-    scalar_IC = 0.5*(1.0+std::tanh((userInputs.domain_size[1]/2.0 - p[1])/(int_width/2))); //* 0.5*(1.0+std::tanh((std::pow(2.0, p[0] - userInputs.domain_size[0]/2.0)
-     //+ std::pow(2.0, p[1] - userInputs.domain_size[1]/2.0) - std::pow(2.0, userInputs.domain_size[1]/6.0))/(int_width/2.0)));
+        // dealii::Utilities::fixed_power<2>()
+    scalar_IC = 0.5*(1.0+std::tanh((userInputs.domain_size[1]/2.0 - p[1])/(int_width/2))) * 0.5 * (1.0+std::tanh((dealii::Utilities::fixed_power<2>(p[0] - userInputs.domain_size[0]/2.0)
+      + dealii::Utilities::fixed_power<2>(p[1] - userInputs.domain_size[1]/2.0) - dealii::Utilities::fixed_power<2>(userInputs.domain_size[0]/6.0))/(int_width/2.0)));
     
     // double r = std::sqrt((p[0]-userInputs.domain_size[0]/2.0)*(p[0]-userInputs.domain_size[0]/2.0)+(p[1]-userInputs.domain_size[1]/2.0)*(p[1]-userInputs.domain_size[1]/2.0));
     // scalar_IC = 0.5*(1.0+std::tanh((r-std::sqrt(userInputs.domain_size[0]/4.0))/(0.5*int_width)));
